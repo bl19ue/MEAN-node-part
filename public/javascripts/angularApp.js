d5ba78d3db70d8e145042059008469194907cec9
Sumit Valecha <svalecha91@gmail.com>
2014-12-31 03:00:01 -0800

    Last

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -16,7 +16,12 @@ angular.module('myNews', ['ui.router'])								//Included UI router same as ng-r
 			.state('news', {
 				url: '/news/{id}',
 				templateUrl: '/news.html',
-				controller: 'OneNewsCtrl'
+				controller: 'OneNewsCtrl',
+				resolve: {										//Now this will help us to get a particular news
+					onenews: ['$stateParams', 'news', function($stateParams, news){	//No need for $stateParams in the NewsCtrl
+						return news.get($stateParams.id);		//Cause this also ensures that when this state is requested
+					}]											//we can get the Id for that news
+				}
 			});
 			 $urlRouterProvider.otherwise('home');					//Finally otherwise() is used so that if any other URL is received, it should always go to state 'home'
 		 }])
@@ -34,6 +39,30 @@ angular.module('myNews', ['ui.router'])								//Included UI router same as ng-r
 				angular.copy(data, o.news);											//angular copy is used cause it will update the UI properly
 			});								
 		};
+		
+		//Now for creating news and persisting them to database we need a query function
+		o.create = function(news){
+			return $http.post('/news', news).success(function(data){
+				o.news.push(data);
+			});
+		};
+		
+		//Upvoting a news
+		o.upvote = function(news){
+			return $http.put('/news/' + news._id + '/upvote').success(function(data){
+				news.upvotes += 1;
+			});
+		};
+		
+		//Get function to return single news from the server
+		o.get = function(id){									
+			console.log("id=" + id);
+			return $http.get('/news/' + id).then(function(res){		//Calling get to retrieve a single news
+				console.log("data=" + res.data);
+				return res.data;									//.then is actually a "promise" in angular which means 
+			});														//that after the GET is completed it is sure to be called
+		};
+		
 		return o;
 	}])
 
@@ -43,21 +72,19 @@ angular.module('myNews', ['ui.router'])								//Included UI router same as ng-r
 		
 		$scope.addNews = function(){								//A function to add a news
 			if(!$scope.title || $scope.title === '') {return;}		//Preventing user to input empty title
-			$scope.news.push({
-				title 	: $scope.title,
-				upvotes	: 0,
-				link	: $scope.link,
-				comments: [
-					{author: 'Sumit', body: 'Good news', upvotes: 0},
-					{author: 'Ken', body: 'Wow!', upvotes: 0}
-				]
-			});	//Now pushing custom news
+			//Default news gone, now we need to call the create method only to add a news to the database
+			
+			news.create({
+				title: $scope.title,
+				link: $scope.link,
+			});
 			$scope.title = '';
+			$scope.link = '';
 		};
 		
-		$scope.increaseVotes = function(n){							//As n means a news only so its property upvotes			
-			n.upvotes += 1;											//directly referenced, hence increasing it will 
-		};															//get reflected on the view
+		$scope.increaseVotes = function(n){							//As n means a news only so its property upvotes		
+			news.upvote(n);											//Now calling the upvote in the service
+		};	
 		
 		
 		
@@ -65,10 +92,11 @@ angular.module('myNews', ['ui.router'])								//Included UI router same as ng-r
 
 	.controller('OneNewsCtrl', [									//Another controller for controlling single news
 	'$scope',
-	'$stateParams',													//StateParams contains parameters passed onto an URL
 	'news',															//Providing the news object
-	function($scope, $stateParams, news){
-		$scope.onenews = news.news[$stateParams.id];				//getting a particular news using stateparams and getting the id parameter from the URL
+	'onenews',
+	function($scope, news, onenews){
+		console.log(onenews);
+		$scope.onenews = onenews;				//getting a particular news from onenews already passed
 		
 		$scope.increaseCommentVotes = function(comment){			//For comment's voting				
 			comment.upvotes += 1;									
